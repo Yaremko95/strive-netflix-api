f3dbee08551304c0d61bde7823962a38c8b11741
tetiana <tetianayaremko@gmail.com>
2020-07-03 13:01:26 +0200

    configures cors

--- src/server.js
+++ src/server.js
@@ -4,7 +4,22 @@ const mediaRouter = require("./media");
 const reviewsRouter = require("./reviews");
 const app = express();
 
-app.use(cors());
+const whitelist =
+  process.env.NODE_ENV === "production"
+    ? [process.env.FE_URL_PROD]
+    : [process.env.FE_URL_DEV];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use("/media", mediaRouter);
 app.use("/reviews", reviewsRouter);
